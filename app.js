51fa14fd5dae2f80abbb5dd382456013bcf15cad
mrhemant <mrhemant000@gmail.com>
2017-12-28 09:13:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://hemant:'hemant@000'@ds133077.mlab.com:33077/social');
+mongoose.connect('ds133077.mlab.com:33077/social');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
