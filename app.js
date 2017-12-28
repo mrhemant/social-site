806f8f6f3be4b537ee13c370429cd9125d4f92c1
mrhemant <mrhemant000@gmail.com>
2017-12-28 09:37:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
 
-mongoose.connect('mongo ds133077.mlab.com:33077/social -u hemant -p hemant@000');
+mongoose.connect('mongo ds133077.mlab.com:33077/social');
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
